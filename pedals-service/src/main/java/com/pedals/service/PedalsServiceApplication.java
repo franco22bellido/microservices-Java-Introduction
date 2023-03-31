6ca4a13ed3047b83fcbaf074358d633b332c1d52
franco22bellido <franco.bellido22@gmail.com>
2023-03-31 02:38:07 -0300

    eureka server y clientes

--- pedals-service/src/main/java/com/pedals/service/PedalsServiceApplication.java
+++ pedals-service/src/main/java/com/pedals/service/PedalsServiceApplication.java
@@ -2,8 +2,11 @@ package com.pedals.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PedalsServiceApplication {
 
 	public static void main(String[] args) {
